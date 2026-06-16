6998d91b808010fb4ce87279e61b19222663bfd9
Guilherme <guiilhermecd@gmail.com>
2026-06-15 22:52:07 -0300

    Criado a entity Role para implementação de segurança

--- src/main/java/com/devsuperior/Gcommerce/entity/User.java
+++ src/main/java/com/devsuperior/Gcommerce/entity/User.java
@@ -2,20 +2,25 @@ package com.devsuperior.Gcommerce.entity;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_user")
-
 public class User {
 
     @Id
@@ -33,6 +38,10 @@ public class User {
 
     @OneToMany(mappedBy = "client")
     private List<Order> orders = new ArrayList<>();
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "tb_user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
 
     public User() {
     }
